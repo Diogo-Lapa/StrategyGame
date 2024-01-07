0161a32256b2dbd5cda5c70bcd836b9076426012
Diogo Lapa <diogomachadolapa@hotmail.com>
2024-01-07 19:23:02 +0000

    Building Data and Base
    Created the building data script and added the base building to the start-up

---
+++ StrategyGame/building_data.gd
@@ -0,0 +1,33 @@
+extends Node
+
+var base = building.new(0, preload("res://Sprites/Base.png"), 0, 0, 0, 0)
+var mine = building.new(1, preload("res://Sprites/Mine.png"), 2, 1, 4, 1)
+var greenhouse = building.new(2, preload("res://Sprites/Greenhouse.png"), 1, 1, 0, 0)
+var solar_panel = building.new(3, preload("res://Sprites/SolarPanel.png"), 4, 1, 0, 0)
+
+class building:
+	
+	#building type
+	var type : int
+	
+	#building texture
+	var icon_texture : Texture
+	
+	#resources the building produces
+	var prod_resources : int = 0
+	var prod_amount : int 
+	
+	#resources the building needs to mantain
+	var upkeep_resources : int = 0
+	var upkeep_amount : int
+	
+	func _init(type, icon_texture, prod_resources, prod_amount, upkeep_resources, upkeep_amount):
+		
+		self.type = type
+		self.icon_texture = icon_texture
+		self.prod_resources = prod_resources
+		self.prod_amount = prod_amount
+		self.upkeep_resources = upkeep_resources
+		self.upkeep_amount = upkeep_amount
+		
+		
--- StrategyGame/map.gd
+++ StrategyGame/map.gd
@@ -4,6 +4,16 @@ var all_tiles : Array
 var tiles_with_buildings : Array
 var tile_size : float = 64.0
 
+func _ready():
+	
+	#when we are initialized, get all of the tiles
+	all_tiles = get_tree().get_nodes_in_group("Tiles")
+	
+	#find the start tile to place the Base Building
+	for x in range(all_tiles.size()):
+		if all_tiles[x].start_tile == true:
+			place_building(all_tiles[x], BuildingData.base.icon_texture)
+
 func get_tile_at_position(position):
 	
 	for x in range (all_tiles.size()):
@@ -34,3 +44,12 @@ func highlight_available_tiles():
 			east_tile.toggle_highlight(true)
 		if west_tile != null:
 			west_tile.toggle_highlight(true)
+			
+
+#places down a building on the map
+func place_building(tile, texture):
+	
+	tiles_with_buildings.append(tile)
+	tile.place_building(texture)
+	
+	disable_tile_highlights()
